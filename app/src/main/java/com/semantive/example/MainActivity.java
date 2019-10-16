72fd89b6a3708567115f3d25f34b314c1ddc1e0f
irwin <zhpngyang52@gmail.com>
2019-10-16 18:20:48 +0800

    Upgrade build env.

--- app/src/main/java/com/semantive/example/MainActivity.java
+++ app/src/main/java/com/semantive/example/MainActivity.java
@@ -1,25 +1,65 @@
 package com.semantive.example;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
 import com.semantive.waveformandroid.waveform.Segment;
 import com.semantive.waveformandroid.waveform.WaveformFragment;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    CustomWaveformFragment mFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkInit();
+    }
+
+    private void checkInit() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+                return;
+            }
+        }
+        init();
+    }
+
+    private void init() {
+        if (mFragment == null) {
+            mFragment = new CustomWaveformFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new CustomWaveformFragment())
+                    .add(R.id.container, mFragment)
                     .commit();
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        init();
     }
 
     public static class CustomWaveformFragment extends WaveformFragment {
@@ -31,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         protected String getFileName() {
-            return "path to your audio file";
+            return new File(Environment.getExternalStorageDirectory(), "conf.mp3").getAbsolutePath();
         }
 
         /**
